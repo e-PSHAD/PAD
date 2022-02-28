f565bae05651599763031fdace4e8829fb8d238d
Simon Denier <simon.denier@epnak.org>
2022-02-28 15:45:06 +0100

    refactor: override Font Awesome icon system in PAD+ theme
    This is a cleaner way to extend/override the Moodle icon system which is used in sidemenu for example.

---
+++ theme/padplus/classes/output/icon_system_fontawesome.php
@@ -0,0 +1,27 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace theme_padplus\output;
+
+class icon_system_fontawesome extends \core\output\icon_system_fontawesome {
+    public function get_core_icon_map() {
+        $iconmap = parent::get_core_icon_map();
+
+        $iconmap['core:i/dashboard'] = 'fa-th-large'; // Override Moodle default fa-tachometer.
+
+        return $iconmap;
+    }
+}
